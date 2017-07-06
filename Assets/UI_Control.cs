3f09ca3e040c4ab8b2db43a14f23fc7f6b4693e1
yunllii <yl3795@nyu.edu>
2017-07-06 13:26:28 -0400

    Add Models and Animations

---
+++ Assets/UI_Control.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Control : MonoBehaviour {
+
+	Animator Anim;
+	bool Once=false;
+	// Use this for initialization
+	void Start () {
+		Anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown("space") && Once == false){
+
+
+			Once = true;
+
+			StartCoroutine(TimedOpen());
+
+
+		}
+
+				
+	}
+
+	IEnumerator TimedOpen(){
+		Anim.SetTrigger ("open_system");
+		yield return new WaitForSeconds(5);
+
+		Debug.Log ("Hahahaha");
+	}
+
+}
